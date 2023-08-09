f1d43340f5753ad90b5980dc71c6bffe8f5a203f
Knoxv <116496469+MonsterKillerrr@users.noreply.github.com>
2023-08-09 22:41:54 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x653c6824f228e2eb8ae9bea78d3745a360ffdf84";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
